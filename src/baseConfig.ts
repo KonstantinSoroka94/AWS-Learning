f58ec12cbb69458fadb05d108a2d2c4eedd67b15
Kostiantyn Soroka <kostiantyn_soroka@epam.com>
2024-06-18 14:45:29 +0300

    database autotests added

--- src/baseConfig.ts
+++ src/baseConfig.ts
@@ -7,4 +7,8 @@ export const BaseConfig = {
   secretAccessKey: process.env.SECRET_ACCESS_KEY,
   region: process.env.REGION,
   accountId: process.env.ACCOUNT_ID,
+  dbUsername: process.env.DB_USERNAME,
+  dbPassword: process.env.DB_PASSWORD,
+  dbName: process.env.DB_NAME,
+  dbPort: process.env.DB_PORT,
 };
